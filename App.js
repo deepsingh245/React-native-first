3fd98f6aa507ca28a1fd888fabaf005946dd3ef8
Deep Singh <deepsingh25244@gmail.com>
2023-08-11 22:35:17 +0530

    second

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
 
 export default function App() {
   return (
     
     <View style={styles.fluid}>
       <View style={styles.container}>
-        <Text style={styles.red}>I can now edit the App.</Text>
+      <Text style={styles.white}>WhatsApp</Text>
+        <View style={{display:'flex',flexDirection:'row',justifyContent:'space-around'}}> 
+        {/* <FontAwesomeIcon icon="fa-solid fa-camera" /> */}
+        <View style={{backgroundColor:'black',height:30,width:30,marginRight:10}}>
+        
+          </View>
+          <View style={{backgroundColor:'black',height:30,width:30,marginRight:10}}></View>
+        <View style={{backgroundColor:'black',height:30,width:30,}}></View>
+        </View>
+        {/* <Text   tyle={styles.red}>I can now edit the App.</Text> */}
       </View>
     </View>
   );
@@ -14,16 +25,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    
-    backgroundColor:'grey',
-    padding:20,
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:"space-between",
+    alignItems:'flex-end',
+    backgroundColor:'#128C7E',
+    padding:10,
+    borderBottomColor:'black',
+    paddingTop:10,
+    height:90,
   },
-  red: {
-    color: "red",
+  white: {
+    color: "white",
     fontSize: 20,
+    
   },
   fluid: {
-    flex:1,
+    // flex:1,
+    // marginTop:20,
   },
 });
